9371ac6692dc2516a580182ae7b43749e69fac7f
Jeffery948 <145318876+Jeffery948@users.noreply.github.com>
2024-05-29 18:29:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ if __name__ == "__main__":
 	
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--env", default='hopper-random-v0')        # OpenAI gym environment name
-	#parser.add_argument("--seed", default=2, type=int)              # Sets Gym, PyTorch and Numpy seeds
+	parser.add_argument("--seed", default=2, type=int)              # Sets Gym, PyTorch and Numpy seeds
 	parser.add_argument("--eval_freq", default=5e3, type=float)     # How often (time steps) we evaluate
 	parser.add_argument("--max_timesteps", default=1e6, type=int)   # Max time steps to run environment or train for (this defines buffer size)
 	parser.add_argument("--start_timesteps", default=25e3, type=int)# Time steps initial random policy is used before training behavioral
@@ -82,25 +82,24 @@ if __name__ == "__main__":
 	parser.add_argument("--phi", default=0.05)                      # Max perturbation hyper-parameter for BCQ
 	args = parser.parse_args()
 
-	for seed in range(3):
-		print("---------------------------------------")	
-		print(f"Setting: Training BCQ, Env: {args.env}, Seed: {seed}")
-		print("---------------------------------------")
+	print("---------------------------------------")	
+	print(f"Setting: Training BCQ, Env: {args.env}, Seed: {args.seed}")
+	print("---------------------------------------")
 
-		if not os.path.exists("./results"):
-			os.makedirs("./results")
-		
-		env = gym.make(args.env)
+	if not os.path.exists("./results"):
+		os.makedirs("./results")
+	
+	env = gym.make(args.env)
 
-		env.seed(seed)
-		env.action_space.seed(seed)
-		torch.manual_seed(seed)
-		np.random.seed(seed)
-		
-		state_dim = env.observation_space.shape[0]
-		action_dim = env.action_space.shape[0] 
-		max_action = float(env.action_space.high[0])
+	env.seed(args.seed)
+	env.action_space.seed(args.seed)
+	torch.manual_seed(args.seed)
+	np.random.seed(args.seed)
+	
+	state_dim = env.observation_space.shape[0]
+	action_dim = env.action_space.shape[0] 
+	max_action = float(env.action_space.high[0])
 
-		device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-		train_BCQ(state_dim, action_dim, max_action, device, args, seed)
+	train_BCQ(state_dim, action_dim, max_action, device, args, arg.seed)
